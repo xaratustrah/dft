a08cd3e94de87b1db7aa734ff54bddc2b25df70c
Xaratustrah <shahab.sanjari@gmail.com>
2016-02-17 21:45:39 +0100

    bump

--- fort_dft.f95
+++ fort_dft.f95
@@ -9,19 +9,19 @@ subroutine dft(n, x, y)
   integer, parameter :: dp = kind(1.0d0)
   real(dp), parameter :: pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628
   integer, intent (in) :: n
-  complex(dp), intent (in), dimension(n) :: x
-  complex(dp), intent (out), dimension(n) :: y
-  real(dp), dimension (n) :: xre, xim, yre, yim
+  complex(dp), intent (in), dimension(0:n-1) :: x
+  complex(dp), intent (out), dimension(0:n-1) :: y
+  real(dp), dimension (0:n-1) :: xre, xim, yre, yim
   real(dp) :: sumreal, sumimag, angle
   integer :: k, t
 
   xre = real(x)
   xim = aimag (x)
 
-  do k = 1, n
+  do k = 0, n-1
      sumreal = 0.0_dp
      sumimag = 0.0_dp
-     do t = 1, n
+     do t = 0, n-1
         angle = 2.0_dp * pi * t * k / n
         sumreal = sumreal + xre(t) * cos(angle) + xim(t) * sin(angle)
         sumimag = sumimag - xre(t) * sin(angle) + xim(t) * cos(angle)
@@ -42,8 +42,9 @@ program test_dft
 
   x = [(1,2), (2,3) ,(3,4), (4, 5)]
   call dft(size(x), x, y)
-  print *, x
-  print *, y
+  print *, 'In:'
+  write(*, *) x
+  print *, 'Out:'
+  write(*,*) y
+
 end program test_dft
-
-
--- py_dft.py
+++ py_dft.py
@@ -5,21 +5,25 @@
 # 2016
 #
 
-import cmath
+#import cmath
+import math
+import numpy as np
 
 
 def dft(x):
     xre = [s.real for s in x]
     xim = [s.imag for s in x]
+
     n = len(xre)
     y = [complex(0, 0)] * n
+    
     for k in range(n):  # For each output element
         sumreal = 0.0
         sumimag = 0.0
         for t in range(n):  # For each input element
-            angle = 2 * cmath.pi * t * k / n
-            sumreal += xre[t] * cmath.cos(angle) + xim[t] * cmath.sin(angle)
-            sumimag += -xre[t] * cmath.sin(angle) + xim[t] * cmath.cos(angle)
+            angle = 2 * math.pi * t * k / n
+            sumreal += xre[t] * math.cos(angle) + xim[t] * math.sin(angle)
+            sumimag += -xre[t] * math.sin(angle) + xim[t] * math.cos(angle)
         y[k] = complex(sumreal, sumimag)
     return y
 
@@ -29,9 +33,11 @@ def dft(x):
 def main():
     x = [complex(1, 2), complex(2, 3), complex(3, 4), complex(4, 5)]
     y = dft(x)
+    print('In:')
     print(x)
+    print('Out:')
     print(y)
-
+    print(np.fft.fft(x))
 
 if __name__ == '__main__':
     main()
